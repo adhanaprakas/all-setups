dc098c80302756cfa722714641325d4a59a3dad7
adhanaprakas <dhanaprakash831@gmail.com>
2024-12-23 18:42:50 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -14,5 +14,5 @@ mv kops-linux-amd64 /usr/local/bin/kops
 aws s3api create-bucket --bucket dhana.k8s.local --region us-east-1
 aws s3api put-bucket-versioning --bucket dhana.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
 export KOPS_STATE_STORE=s3://dhana.k8s.local
-kops create cluster --name rahams.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.medium
-kops update cluster --name rahams.k8s.local --yes --admin
+kops create cluster --name prakash.k8s.local --zones us-east-1a,us-east-1b --master-size t2.medium --master-count 1 --node-size t2.micro --node-count 2
+kops update cluster --name prakash.k8s.local --yes --admin
